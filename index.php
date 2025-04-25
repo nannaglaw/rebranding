5df65a00daafddfe854ae5085c39e43033c7d4d2
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-04-25 15:23:49 +0200

    tilføjet billed og mappe

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ require "settings/init.php";
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
     
-    <link href="css/styles.scss" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<body class="bg-lb">
+<body class="bg-wh">
 
 
 
