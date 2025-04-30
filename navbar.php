b657c9f330457cba3b1178bd33023efca946a023
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-04-30 11:03:33 +0200

    navbar

--- navbar.php
+++ navbar.php
@@ -1,10 +1,11 @@
 
 <link href="css/styles.css" rel="stylesheet">
+<a class="" href="index.php"><img src="img/LogoTheia.webp" class="img-fluid mx-auto d-block" style="max-width:25%;"></a>
 
-<nav class="navbar navbar-expand-lg bg-body-wh">
+<nav class="navbar navbar-expand-lg bg-wh">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -25,3 +26,5 @@
         </div>
     </div>
 </nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
