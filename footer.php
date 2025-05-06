711afdeff93de1ad9daa68da2ea61135f7a66f4c
amalie <amp001@edu.zealand.dk>
2025-05-06 12:48:05 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,18 +1,30 @@
 <link href="css/styles.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-<div class="container pt-5 bg-wh">
-    <div class="row">
-        <div class="col-md-6 col-lg-6">
+<div class="container pt-5 bg-wh ">
+    <div class="row p-5 border-lb border-top">
+        <div class="col-12 col-md-6 col-lg-6">
+            <div class="d-flex gap-4">
             <?php include "bookKnap.php";?>
             <?php include "behandlingKnap.php";?>
-            <p class="mt-5">Telefon: +45 61 47 73 00 </p>
-            <p>Instagram: @Salon_Theia</p>
-                <p>Adresse: Skovsøgade 9,
+            </div>
+            <div class="ps-5">
+                <p class="mt-5"><span class="fw-medium">Telefon:</span>+45 61 47 73 00 </p>
+                <p><span class="fw-medium">Instagram:</span> @Salon_Theia</p>
+                <p><span class="fw-medium">Adresse:</span> Skovsøgade 9,
                     4200 Slagelse</p>
-                <p>CVR: 44130475</p>
+                <p><span class="fw-medium">CVR:</span> 44130475</p>
+            </div>
         </div>
-        <div class="col-md-6 col-lg-6">
-            <p class="text-lb fs-4 fw-medium">Åbningstider:</p>
+        <div class="col-12 col-md-6 col-lg-6">
+            <p class="text-lb fs-4 fw-medium ">Åbningstider:</p>
+            <p>Åbningstiderne kan variere, tjek derfor booking <a class="link-gr" href="https://klinik-skoen.planway.com/">her.</a> </p>
+            <p class="m-1"><span class="fw-medium">Mandag:</span> 9.30 - 17.30 </p>
+            <p class="m-1"><span class="fw-medium">Tirsdag:</span> 9.30 - 17.30</p>
+            <p class="m-1"><span class="fw-medium">Onsdag:</span> 9.30 - 17.30 </p>
+            <p class="m-1"><span class="fw-medium">Torsdag:</span> 9.30 - 20.00</p>
+            <p class="m-1"><span class="fw-medium">Fredag:</span> 9.30 - 17.30</p>
+            <p class="m-1"><span class="fw-medium">Lørdag:</span> Efter aftale</p>
+            <p class="m-1"><span class="fw-medium">Søndag:</span> LUKKET</p>
         </div>
     </div>
