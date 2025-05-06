a781f9db6034d4bda63d56c85ae2bcb42a941619
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-06 10:44:01 +0200

    knapper

--- behandlinger.php
+++ behandlinger.php
@@ -25,6 +25,8 @@ require "settings/init.php";
 
 <?php include "navbar.php"; ?>
 
+<?php include "kontaktKnap.php";?>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
