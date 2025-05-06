f13d51e313f3ca9cba0fe38af614b49579229c31
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-06 10:11:13 +0200

    book knap

--- bookKnap.php
+++ bookKnap.php
@@ -2,4 +2,4 @@
 <link href="css/styles.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-<button type="button" class="btn btn-lb btn-lg text-wh fw-medium rounded-5 px-5 py-2" style="font-family: Zodiak" href="https://klinik-skoen.planway.com/">Book tid</button>
+<button type="button" class="btn btn-lb btn-lg text-wh fw-medium rounded-5 px-5 py-2" style="font-family: Zodiak" onclick="location.href='https://klinik-skoen.planway.com/'">Book tid</button>
