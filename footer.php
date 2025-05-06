bd52312a1b2d119b3b1bbef561ad8a574fc033ac
amalie <amp001@edu.zealand.dk>
2025-05-06 11:24:34 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<link href="css/styles.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<div class="container pt-5 bg-wh">
+    <div class="row">
+        <div class="col-md-6 col-lg-6">
+            <?php include "bookKnap.php";?>
+            <?php include "behandlingKnap.php";?>
+            <p class="mt-5">Telefon: +45 61 47 73 00 </p>
+            <p>Instagram: @Salon_Theia</p>
+                <p>Adresse: Skovsøgade 9,
+                    4200 Slagelse</p>
+                <p>CVR: 44130475</p>
+        </div>
+        <div class="col-md-6 col-lg-6">
+            <p class="text-lb fs-4 fw-medium">Åbningstider:</p>
+        </div>
+    </div>
