461e8d334e720fc613d8ef3c81c43ea2f4b949aa
amalie <amp001@edu.zealand.dk>
2025-05-06 09:54:53 +0200

    Behandlings knap

--- behandlingKnap.php
+++ behandlingKnap.php
@@ -1,1 +1,4 @@
-<?php
+<link href="css/styles.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<button type="button" class="btn btn-gr btn-lg fw-bold text-wh rounded-5 px-5 py-2">Behandlinger</button>
