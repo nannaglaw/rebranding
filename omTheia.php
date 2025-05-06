4530f682c60fba93bd3909ea70795dc14f5ce7e9
amalie <amp001@edu.zealand.dk>
2025-05-06 13:46:34 +0200

    Om theia underside

--- omTheia.php
+++ omTheia.php
@@ -25,6 +25,32 @@ require "settings/init.php";
 
 <?php include "navbar.php"; ?>
 
+<div class="container">
+    <div class="row">
+        <div class="col"></div>
+        <div class="col-8 fs-5 text-db">
+            <p class="m-3 px-5 text-center text-lb display-5 fw-medium">Om Skønhedssalon Theia</p>
+            <p class=" m-3 px-5">Vi startede vores rejse i 2022 under navnet <span
+                        class="fw-medium">Klinik S.K.Ø.N</span>, og vi er stolte af den udvikling, vi har gennemgået
+                siden da. I takt med at vi voksede, blev det tid til forandring: nyt navn, nye rammer, samme passion.
+            </p>
+            <p class="m-3 px-5 "> Vi har skiftet navn fra <span class="fw-medium">Klinik S.K.Ø.N</span> til
+                Skønhedssalon THEIA og er flyttet lige ind ved siden af den gamle adresse, så nu har vi lokale på
+                <a class="link-gr fw-medium"
+                   href="https://www.google.com/maps/place/Skovs%C3%B8gade+9,+4200+Slagelse/@55.4032068,11.3544408,17z/data=!3m1!4b1!4m6!3m5!1s0x464d617c0ddf17b5:0x4e6fdabb9a326701!8m2!3d55.4032038!4d11.3570211!16s%2Fg%2F11c19xck96?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D">
+                    Skovsøgade 9, 4200 Slagelse. </a>
+                Her har vi skabt en endnu mere indbydende og afslappende atmosfære, hvor du kan forkæle dig selv og
+                finde ro fra hverdagens travlhed.</p>
+            <p class="m-3 px-5">Vores mission, som hos <span class="fw-medium">Klinik S.K.Ø.N</span>, er fortsat den
+                samme – at give dig en <span class="fst-italic">personlig, professionel og skøn oplevelse</span> hver
+                gang du besøger os.
+            <p class="text-center fw-medium fs-4">Vi glæder os til at byde dig velkommen i vores nye omgivelser!</p>
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
