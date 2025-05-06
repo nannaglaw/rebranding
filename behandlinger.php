33b5a34abb1aa4cf66c0f91f9159b73e2cbca34c
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-06 14:34:09 +0200

    body typografi

--- behandlinger.php
+++ behandlinger.php
@@ -31,29 +31,28 @@ require "settings/init.php";
 
 <div class="container">
     <div class="row justify-content-center">
-        <p class="overskrift text text-lb fw-medium fs-4 text-center">Brow- & Lash lift</p>
-            <div class="card col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+        <div class="card col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
             </div>
-            <div class="card col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                        card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+        </div>
+        <div class="card col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
             </div>
         </div>
     </div>
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
