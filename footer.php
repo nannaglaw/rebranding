3ead7ce5398b3e620ac83059ed43958fee989f3d
amalie <amp001@edu.zealand.dk>
2025-05-13 10:48:29 +0200

    footer

--- footer.php
+++ footer.php
@@ -5,31 +5,34 @@
 <div class="container pt-5 bg-wh">
     <div class="p-5 border-lb border-top">
         <div class="row">
-        <div class="col col-md-6 col-lg-6">
-            <div class="d-flex gap-4">
-            <?php include "bookKnap.php";?>
-            <?php include "behandlingKnap.php";?>
-
+            <div class="col-12 col-lg-6">
+                <div class="d-flex gap-4">
+                    <?php include "bookKnap.php"; ?>
+                    <?php include "behandlingKnap.php"; ?>
+                </div>
+                <div class="ps-lg-5 pb-4">
+                    <p class="mt-5 text-lb fs-4 fed mb-2">Kontakt</p>
+                    <p><span class="fed">Telefon:</span>+45 61 47 73 00 </p>
+                    <p><span class="fed">Instagram:</span> @Salon_Theia</p>
+                    <p><span class="fed">Adresse:</span> Skovsøgade 9,
+                        4200 Slagelse</p>
+                    <p><span class="fed">CVR:</span> 44130475</p>
+                </div>
             </div>
-            <div class="ps-5">
-                <p class="mt-5"><span class="fw-medium">Telefon:</span>+45 61 47 73 00 </p>
-                <p><span class="fw-medium">Instagram:</span> @Salon_Theia</p>
-                <p><span class="fw-medium">Adresse:</span> Skovsøgade 9,
-                    4200 Slagelse</p>
-                <p><span class="fw-medium">CVR:</span> 44130475</p>
+            <div class="col-12 col-lg-6">
+                <p class="text-lb fs-4 fed mb-0">Åbningstider:</p>
+                <p>Åbningstiderne kan variere, tjek derfor booking <a class="link-gr fed"
+                                                                      href="https://klinik-skoen.planway.com/">her.</a>
+                </p>
+                <p class="mb-2"><span class="fed">Mandag:</span> 9.30 - 17.30 </p>
+                <p class="mb-2"><span class="fed">Tirsdag:</span> 9.30 - 17.30</p>
+                <p class="mb-2"><span class="fed">Onsdag:</span> 9.30 - 17.30 </p>
+                <p class="mb-2"><span class="fed">Torsdag:</span> 9.30 - 20.00</p>
+                <p class="mb-2"><span class="fed">Fredag:</span> 9.30 - 17.30</p>
+                <p class="mb-2"><span class="fed">Lørdag:</span> Efter aftale</p>
+                <p class="mb-2"><span class="fed">Søndag:</span> LUKKET</p>
             </div>
         </div>
-        <div class="col col-md-6 col-lg-6">
-            <p class="text-lb fs-4 fw-medium ">Åbningstider:</p>
-            <p>Åbningstiderne kan variere, tjek derfor booking <a class="link-gr" href="https://klinik-skoen.planway.com/">her.</a> </p>
-            <p class="m-1"><span class="fw-medium">Mandag:</span> 9.30 - 17.30 </p>
-            <p class="m-1"><span class="fw-medium">Tirsdag:</span> 9.30 - 17.30</p>
-            <p class="m-1"><span class="fw-medium">Onsdag:</span> 9.30 - 17.30 </p>
-            <p class="m-1"><span class="fw-medium">Torsdag:</span> 9.30 - 20.00</p>
-            <p class="m-1"><span class="fw-medium">Fredag:</span> 9.30 - 17.30</p>
-            <p class="m-1"><span class="fw-medium">Lørdag:</span> Efter aftale</p>
-            <p class="m-1"><span class="fw-medium">Søndag:</span> LUKKET</p>
-        </div>
     </div>
-    </div>
+</div>
 </body>
