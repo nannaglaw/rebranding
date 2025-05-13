37550f9f59d1cfe2673abc7e1482671f61a7052c
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-13 11:20:27 +0200

    Behandlinger

--- behandlinger.php
+++ behandlinger.php
@@ -33,13 +33,13 @@ require "settings/init.php";
             <div class="accordion" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item">
                     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false"
                                 aria-controls="panelsStayOpen-collapseOne">
                             Accordion Item #1
                         </button>
                     </h2>
-                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse"
                          aria-labelledby="panelsStayOpen-headingOne">
                         <div class="accordion-body">
                             <strong>This is the first item's accordion body.</strong> It is shown by default, until the
@@ -68,6 +68,30 @@ require "settings/init.php";
                         <div class="accordion-body">
                             <strong>This is the second item's accordion body.</strong> It is hidden by default, until
                             the collapse plugin adds the appropriate classes that we use to style each element. These
+                            classes control the overall appearance, as well as the showing and hiding via CSS
+                            transitions. You can modify any of this with custom CSS or overriding our default variables.
+                            It's also worth noting that just about any HTML can go within the
+                            <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-6 offset-lg-3 mt-lg-4 mt-md-4">
+            <div class="accordion" id="accordionPanelsStayOpenExample">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false"
+                                aria-controls="panelsStayOpen-collapseThree">
+                            Accordion Item #3
+                        </button>
+                    </h2>
+                    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
+                         aria-labelledby="panelsStayOpen-headingThree">
+                        <div class="accordion-body">
+                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the
+                            collapse plugin adds the appropriate classes that we use to style each element. These
                             classes control the overall appearance, as well as the showing and hiding via CSS
                             transitions. You can modify any of this with custom CSS or overriding our default variables.
                             It's also worth noting that just about any HTML can go within the
--- bookKnap.php
+++ bookKnap.php
@@ -4,4 +4,3 @@
 
 <button type="button" class="btn btn-lb btn-lg text-wh fw-medium rounded-5 px-5 py-2" style="font-family: Zodiak" onclick="location.href='https://klinik-skoen.planway.com/'">Book tid</button>
 
-
