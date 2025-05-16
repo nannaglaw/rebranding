2c466de691f803cb5bec82f00f00979ab677749f
amalie <amp001@edu.zealand.dk>
2025-05-16 13:16:10 +0200

    footer

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ require "settings/init.php";
     </div>
 </div>
 </div>
-
+<?php include "footer.php"; ?>
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
