ef4a18688ffbeabb9440b83d22a4a2440687a70b
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-16 12:11:39 +0200

    om theia

--- omTheia.php
+++ omTheia.php
@@ -27,50 +27,63 @@ require "settings/init.php";
 
 <div class="container">
     <div class="row">
-        <div class="d-flex justify-content-center align-items-center">
-        <div class="col-12 col-lg-8 fs-5 text-db">
-            <p class="m-3 px-5 text-lg-center text-lb display-5 fw-semibold">Om Skønhedssalon THEIA</p>
-            <p class=" m-3 px-5">Vi startede vores rejse i 2022 under navnet <span
-                        class="fw-semibold">Klinik S.K.Ø.N</span>, og vi er stolte af den udvikling, vi har gennemgået
-                siden da. I takt med at vi voksede, blev det tid til forandring: nyt navn, nye rammer, samme passion.
-            </p>
-            <p class="m-3 px-5 "> Vi har skiftet navn fra <span class="fw-semibold">Klinik S.K.Ø.N</span> til
-                Skønhedssalon THEIA og er flyttet lige ind ved siden af den gamle adresse, så nu har vi lokale på
-                <a class="link-gr fw-medium"
-                   href="https://www.google.com/maps/place/Skovs%C3%B8gade+9,+4200+Slagelse/@55.4032068,11.3544408,17z/data=!3m1!4b1!4m6!3m5!1s0x464d617c0ddf17b5:0x4e6fdabb9a326701!8m2!3d55.4032038!4d11.3570211!16s%2Fg%2F11c19xck96?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D">
-                    Skovsøgade 9, 4200 Slagelse. </a>
-                Her har vi skabt en endnu mere indbydende og afslappende atmosfære, hvor du kan forkæle dig selv og
-                finde ro fra hverdagens travlhed.</p>
-            <p class="m-3 px-5">Vores mission, som hos <span class="fw-semibold">Klinik S.K.Ø.N</span>, er fortsat den
-                samme – at give dig en <span class="fst-italic">personlig, professionel og skøn oplevelse</span> hver
-                gang du besøger os.
-            <p class="text-center fw-semibold fs-4">Vi glæder os til at byde dig velkommen i vores nye omgivelser!</p>
-        </div>
-        </div>
+        <div class="justify-content-center align-items-center">
+
+            <p class="text text-lb ps-lg-5 fw-medium display-5 text-center mt-5 mb-3" style="font-family: Zodiak">Om Skønhedssalon THEIA</p>
+
+            <div class="col-12 col-lg-10 col-md-10 fs-5 text-db">
+                <p class="col-12 offset-lg-4 col-lg-7 offset-md-3 col-md-10 fs-5 text-db">Vi startede vores rejse i 2022 under navnet <span
+                            class="fw-semibold">Klinik S.K.Ø.N</span>, og vi er stolte af den udvikling, vi har
+                    gennemgået
+                    siden da. I takt med at vi voksede, blev det tid til forandring: nyt navn, nye rammer, samme
+                    passion.
+                </p>
+                <p class="col-12 offset-lg-4 col-lg-7 offset-md-3 col-md-10 fs-5 text-db"> Vi har skiftet navn fra <span class="fw-semibold">Klinik S.K.Ø.N</span> til
+                    Skønhedssalon THEIA og er flyttet lige ind ved siden af den gamle adresse, så nu har vi lokale på
+                    <a class="link-gr fw-medium"
+                       href="https://www.google.com/maps/place/Skovs%C3%B8gade+9,+4200+Slagelse/@55.4032068,11.3544408,17z/data=!3m1!4b1!4m6!3m5!1s0x464d617c0ddf17b5:0x4e6fdabb9a326701!8m2!3d55.4032038!4d11.3570211!16s%2Fg%2F11c19xck96?entry=ttu&g_ep=EgoyMDI1MDQzMC4xIKXMDSoASAFQAw%3D%3D">
+                        Skovsøgade 9, 4200 Slagelse. </a>
+                    Her har vi skabt en endnu mere indbydende og afslappende atmosfære, hvor du kan forkæle dig selv og
+                    finde ro fra hverdagens travlhed.</p>
+                <p class="col-12 offset-lg-4 col-lg-7 offset-md-3 col-md-10 fs-5 text-db">Vores mission, som hos <span class="fw-semibold">Klinik S.K.Ø.N</span>, er fortsat
+                    den
+                    samme – at give dig en <span class="fst-italic">personlig, professionel og skøn oplevelse</span>
+                    hver
+                    gang du besøger os.
+                <p class="text-center fw-semibold fs-4 col-md-12 col-lg-9 offset-lg-3 offset-md-2">Vi glæder os til at byde dig velkommen i vores nye
+                    omgivelser!</p>
+            </div>
         </div>
     </div>
+</div>
 
 <div class="container-fluid bg-wd">
     <div class="row">
         <div class="col-lg-4 offset-lg-2 col-12 col-md-4">
             <img class="ms-5 ms-lg-0 ms-md-0 my-5 img-fluid" src="img/profil.webp" alt="Billede af Charlotte Larsen">
         </div>
-        <div class="col-12 col-lg-4 col-md-8">
-            <p class="text-center text-lb display-6 pt-lg-5 pt-3 m-0" style="font-family:'Zodiak-Bold'">Mød behandleren</p>
-            <p class="text-center text-lb display-5 "style="font-family:'Zodiak-Italic'">Charlotte Larsen</p>
-            <p class="fs-5 text-db pb-5 px-4 text-start">Bag <span style="font-family:'Zodiak-Bold' ">Skønhedssalon THEIA</span> står en passioneret og professionel behandler, der er certificeret lash- og browlift-stylist
-                med fokus på smukke, holdbare resultater. Derudover er hun certificeret i Shellac-behandlinger og arbejder med stor omhu for
-                at sikre både æstetik og kvalitet i hver eneste behandling. Med faglig stolthed og øje for detaljen skabes en tryg og personlig
+        <div class="col-12 col-lg-6 col-md-8">
+            <p class="text-center text-lb display-6 pt-lg-5 pt-3 m-0" style="font-family:'Zodiak-Bold'">Mød
+                behandleren</p>
+            <p class="text-center text-lb display-5 " style="font-family:'Zodiak-Italic'">Charlotte Larsen</p>
+            <div class="col-lg-9 offset-lg-2">
+            <p class="fs-5 text-db pb-5 px-4 text-start">Bag <span style="font-family:'Zodiak-Bold' ">Skønhedssalon THEIA</span>
+                står en passioneret og professionel behandler, der er certificeret lash- og browlift-stylist
+                med fokus på smukke, holdbare resultater. Derudover er hun certificeret i Shellac-behandlinger og
+                arbejder med stor omhu for
+                at sikre både æstetik og kvalitet i hver eneste behandling. Med faglig stolthed og øje for detaljen
+                skabes en tryg og personlig
                 oplevelse <span style="font-family: 'Zodiak-BoldItalic'">– hver gang.</span></p>
+            </div>
             <div class="text-center m-0">
-            <?php include "kontaktKnap.php";?>
-        </div>
+                <?php include "kontaktKnap.php"; ?>
+            </div>
         </div>
     </div>
 </div>
 
 
-        <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
