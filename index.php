c9d25b951d4068964afbea5d1fbe8c715a950235
amalie <amp001@edu.zealand.dk>
2025-05-19 16:15:59 +0200

    forside

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ require "settings/init.php";
         <div class="col-12 text-overlay text-center position-relative">
             <p class="text text-lb m-0 extra display-2">Velkommen til </p>
             <p class="text text-lb m-0 extra display-2">Skønhedssalon THEIA</p>
-            <p class="text text-lb m-0 kursiv display-2">– din oase af skønhed og ro</p>
+            <p class="text text-lb m-0 kursiv display-3">– din oase af skønhed og ro</p>
 
             <p class="text-overlay text-db mt-3 fs-3" style="font-family: Zodiak">
                 Eksklusive behandlinger i rolige omgivelser – <br> negle, vipper & bryn
@@ -61,21 +61,21 @@ require "settings/init.php";
 <div class="container-fluid bg-wd">
     <div class="row">
         <div class="col-12 col-lg-5 offset-lg-1">
-            <img class="img-fluid ms-5 ms-lg-0 ms-md-0 my-5 rounded-3" src="img/behandler.webp" alt="Billede af Charlotte Larsen">
+            <img class="img-fluid ms-lg-0 ms-md-0 my-lg-5 mt-5 rounded-3" src="img/behandler.webp" alt="Billede af Charlotte Larsen">
         </div>
-        <div class="col-12 col-lg-6 px-5 ">
+        <div class="col-12 col-lg-6 px-lg-5 ">
             <p class="text-center text-lb display-6 pt-lg-5 pt-3 m-0 fed">Mød
                 behandleren</p>
             <p class="text-center text-lb display-5 kursiv">Charlotte Larsen</p>
-            <p class="fs-5 text-db px-5">Velkommen til min skønhedssalon.
+            <p class="fs-5 text-db px-4 px-lg-5">Velkommen til min skønhedssalon.
                 Mit navn er Charlotte Larsen, og jeg er indehaver af Skønhedssalon THEIA. Hvis du vil vide mere om mig
                 og min salon kan du læse mere <a class="link-gr fed"
                                                  href="omTheia.php">her.</a></p>
             <div class="d-flex justify-content-center align-items-center"><img class="img-fluid pt-3 pb-2"
                                                                                src="img/ii.webp" alt="ii"></div>
-            <p class="fs-5 text-db px-5 pb-3">Jeg brænder for at fremhæve din naturlige skønhed med faglighed, præcision
+            <p class="fs-5 text-db px-4 px-lg-5 pb-3 text-center">Jeg brænder for at fremhæve din naturlige skønhed med faglighed, præcision
                 og æstetisk sans – altid med respekt for dig og dit udtryk.</p>
-            <div class="d-flex justify-content-center mb-3 mb-md-0 gap-lg-5 gap-2">
+            <div class="d-flex justify-content-center mb-5 mb-md-0 gap-lg-5 gap-2">
                 <?php include "omTheiaKnap.php"; ?>
                 <?php include "kontaktKnap.php"; ?>
             </div>
@@ -98,7 +98,7 @@ require "settings/init.php";
         <div class="rounded rounded-2 col-12 col-lg-4 col-md-4 text-center mt-5 d-flex justify-content-center">
             <a class="btn btn-wd knapperne p-4 px-5 " href="behandlinger.php" role="button"><img
                         src="img/eyebrows.gif"
-                        alt="øjnbryn animation"
+                        alt="øjenbryn animation"
                         style="width:75px;height:75px;">
                 <p class="text text-db fs-3">Bryn</p></a>
         </div>
