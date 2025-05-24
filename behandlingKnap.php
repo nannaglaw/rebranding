702208b6c1f7db2524f3eb0eb37036aa8249951b
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-24 16:22:52 +0200

    knapper

--- behandlingKnap.php
+++ behandlingKnap.php
@@ -1,4 +1,4 @@
 
 
 
-<button type="button" class="btn btn-outline-db rounded-4 text-lb btn-lg fw-medium px-lg-5 py-lg-2" style="font-family: Zodiak" onclick="location.href='behandlinger.php'">Behandlinger</button>
+<button type="button" class="btn btn-outline-db rounded-4 border-2 text-lb btn-lg fw-medium px-lg-5 py-lg-2" style="font-family: Zodiak" onclick="location.href='behandlinger.php'">Behandlinger</button>
