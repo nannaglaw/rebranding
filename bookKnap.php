06ea3a96c5566f41ca140172245095c829df73ee
Nannaglawnielsen <nan002@edu.zealand.dk>
2025-05-24 16:22:26 +0200

    knapper

--- bookKnap.php
+++ bookKnap.php
@@ -1,4 +1,3 @@
-
-
-<button type="button" class="btn btn-outline-gr text-gr rounded-4 btn-lg fw-medium px-lg-5 py-lg-2 px-4" style="font-family: Zodiak" onclick="location.href='https://klinik-skoen.planway.com/'">Book tid</button>
-
+<button type="button" class="btn btn-outline-gr text-gr rounded-4 border-2 btn-lg fw-medium px-lg-5 py-lg-2 px-4"
+        style="font-family: Zodiak" onclick="location.href='https://klinik-skoen.planway.com/'">Book tid
+</button>
--- prislisteKnap.php
+++ prislisteKnap.php
@@ -1,2 +1,2 @@
 
-<button type="button" class="btn btn-outline-db rounded-4 text-lb btn-lg fw-medium px-5 py-2 fs-4" style="font-family: Zodiak" onclick="location.href='prisliste.php'">Prisliste</button>
+<button type="button" class="btn btn-outline-db rounded-4 border-2 text-lb btn-lg fw-medium px-5 py-2 fs-4" style="font-family: Zodiak" onclick="location.href='prisliste.php'">Prisliste</button>
